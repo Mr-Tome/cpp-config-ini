fe94067776837390e304c5431416621a373fb5b4
unknown <mistourt@gmail.com>
2024-06-02 13:19:31 -0500

    configure now downloads and a specific cmake version to the specified directory within HOME. Also there is a clean argument for configure to remove all the dependencies

--- scripts/configure_cmake.sh
+++ scripts/configure_cmake.sh
@@ -1,28 +1,39 @@
 #!/bin/bash
 
-echo "Downloading CMake v3.29.3"
+cmake_ver=3.29.3
+op_sys=windows-x86_64
 
-# Define the URL of the CMake installer
-url="https://github.com/Kitware/CMake/releases/download/v3.29.3/cmake-3.29.3-windows-x86_64.zip"
-
-# Define the temporary file to store the downloaded installer
-zip_file=$(mktemp)
-
-# Download the CMake installer
-curl -L -o $zip_file $url
-
+#let's check the final destination before we actually do anything
 destination="$HOME/.configuration-dependencies/differential-equations"
+destination_dep="$destination/cmake-$cmake_ver-$op_sys"
 
-# Check if the destination directory exists; if not, create it
-if [[! -d $destination ]]; then
-	echo "making $destination"
-    mkdir -p $destination
+if [ -d $destination_dep ]; then
+	echo "CMake $cmake_ver-$op_sys already exists...skipping"
+else
+	echo "Downloading CMake v$cmake_ver"
+	# Define the URL of the CMake installer
+	url="https://github.com/Kitware/CMake/releases/download/v$cmake_ver/cmake-$cmake_ver-$op_sys.zip"
+
+	# Define the temporary file to store the downloaded installer
+	zip_file=$(mktemp)
+	
+	# Download the CMake installer
+	curl -L -o $zip_file $url
+
+	# Check if the destination directory exists; if not, create it
+	if [! -d $destination ]; then
+		echo "making $destination"
+		mkdir -p $destination
+	else
+		echo "$destination exists"
+	fi
+	echo "Extracting the ZIP archive to the aforementioned destination directory."
+	unzip -q $zip_file -d $
+	echo "CMake ZIP archive extracted to $destination."
+
+	# Clean up the ZIP file
+	echo "Cleaning up the downloaded ZIP file" 
+	rm $zip_file
 fi
 
-# Extract the ZIP archive to the specified destination directory
-unzip -q $zip_file -d $destination
 
-# Clean up the ZIP file
-rm $zip_file
-
-echo "CMake ZIP archive extracted to $destination."
