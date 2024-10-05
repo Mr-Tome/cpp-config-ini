ae83891a23d90e27e0739b0d4cb58db906262aec
unknown <mistourt@gmail.com>
2024-10-05 17:48:48 -0500

    updating some headers to prepare for better handling of type and default value

--- source/config_library/config_reader.hpp
+++ source/config_library/config_reader.hpp
@@ -10,108 +10,132 @@
 
 namespace ConfigLib {
    namespace ConfigGen {
-       struct ConfigItem {
-           const char* name;
+	   
+		class ConfigItemBase {
+		public:
+			virtual ~ConfigItemBase() = default;
+			virtual std::string getName() const = 0;
+			virtual std::string getType() const = 0;
+			virtual std::string getDefaultValueString() const = 0;
+			virtual std::string getDescription() const = 0;
+			virtual const ValidationRules::Rule* getValidationRule() const = 0;
+		};
+		
+		template<typename T>
+		class ConfigItem : public ConfigItemBase {
+		public: 
+           const std::string name;
            const char* type;
-           const char* defaultValue;
-           const char* description;
+           const T defaultValue;
+           const std::string description;
            const ValidationRules::Rule* validationRule;
-       };
-
-       struct ConfigSection {
-           std::string name;
-           std::vector<ConfigItem> items;
-       };
+		   
+		   ConfigItem(
+				std::string n,
+				T defVal,
+				std::string desc,
+				const ValidationRules::Rule* rule = nullptr)
+				: name(std::move(n)),
+				defaultValue(std::move(defVal)),
+				description(std::move(desc)),
+				validationRule(rule) {}
+				
+			const std::string getName() const override { return name; }
+			const std::string getDescription() const override { return description; }
+			const ValidationRules::Rule* getValidationRule() const override { return validationRule; }
+			
+			std::string getType() const override {
+				if (std::is_same<T, int>::value) return "int";
+				if (std::is_same<T, double>::value) return "double";
+				if (std::is_same<T, std::string>::value) return "string";
+				if (std::is_same<T, std::vector<double>>::value) return "vector<double>";
+				return "unknown type";
+			}
+			
+			std::string getDefaultValueString() const override {
+				std::ostringstream oss;
+				oss << defaultValue;
+				return oss.str();
+			}
+		};
+		
+		// Specialization for std::vector<double>
+		template<>
+		inline std::string ConfigItem<std::vector<double>>::getDefaultValueString() const {
+			std::ostringstream oss;
+			for (size_t i = 0; i < defaultValue.size(); ++i) {
+				if (i > 0) oss << ",";
+				oss << defaultValue[i];
+			}
+			return oss.str();
+		}
+	
+        struct ConfigSection {
+            std::string name;
+			std::vector<std::unique_ptr<ConfigItemBase>> items;
+			
+			template<typename... Args>
+			ConfigSection(std::string n, Args&&... args) : name(std::move(n)) {
+				(addItem(std::forward<Args>(args)), ...);
+			}
+		private:
+			template<typename T, typename... NextItem>
+			void addItem(const std::string& name, T&& defaultValue, const std::string& description, const ValidationRules::Rule* rule, NextItem&&... nextItem) {
+				items.push_back(std::make_unique<ConfigItem<std::decay_t<T>>>(name, std::forward<T>(defaultValue), description, rule));
+				if constexpr (sizeof...(nextItem) > 0) {
+					addItem(std::forward<NextItem>(nextItem)...);
+				}
+			}
+		
+			template<typename T, typename... NextItem>
+			void addItem(const std::string& name, T&& defaultValue, const std::string& description, NextItem&&... nextItem) {
+				items.push_back(std::make_unique<ConfigItem<std::decay_t<T>>>(name, std::forward<T>(defaultValue), description, nullptr));
+				if constexpr (sizeof...(nextItem) > 0) {
+					addItem(std::forward<NextItem>(nextItem)...);
+				}
+			}
+		};
 
        bool validateConfig(const std::vector<ConfigSection>& sections);
        std::string generateConfig(const std::vector<ConfigSection>& sections);
    }
 
-   class ConfigValue {
-   public:
-       virtual ~ConfigValue() = default;
-       virtual std::string toString() const = 0;
-       virtual void fromString(const std::string& str) = 0;
-       virtual std::shared_ptr<ConfigValue> clone() const = 0;
-   };
-
-   template<typename T>
-   class TypedConfigValue : public ConfigValue {
-   public:
-       TypedConfigValue(const T& val);
-       const T& getValue() const;
-       void setValue(const T& val);
-       std::string toString() const override;
-       void fromString(const std::string& str) override;
-       std::shared_ptr<ConfigValue> clone() const override;
-
-   private:
-       T value;
-   };
-
-   //using ValidationRule = std::function<bool(const ConfigValue&)>;
-
-class ConfigSection {
-public:
-    template<typename T>
-    void setValue(const std::string& key, const T& value);
-
-    void setValue(const std::string& key, const std::string& value);
-    void setValue(const std::string& key, const std::vector<double>& value);
-
-    template<typename T>
-    T getValue(const std::string& key) const;
-
-    bool hasKey(const std::string& key) const;
-    void setValidationRule(const std::string& key, const ValidationRules::Rule* rule);
-    const std::unordered_map<std::string, std::shared_ptr<ConfigValue>>& getValues() const;
-
-private:
-    std::unordered_map<std::string, std::shared_ptr<ConfigValue>> values;
-    std::unordered_map<std::string, const ValidationRules::Rule*> validationRules;
-};
-
-class ConfigReader {
-public:
-    ConfigReader();
-    virtual ~ConfigReader() = default;
-
-	void initialize();
-
-    template<typename T>
-    T getValue(const std::string& section, const std::string& key) const;
-
-    template<typename T>
-    void setValue(const std::string& section, const std::string& key, const T& value);
-
-    void setValue(const std::string& section, const std::string& key, const std::string& value);
-    void setValue(const std::string& section, const std::string& key, const std::vector<double>& value);
-
-    bool hasValue(const std::string& section, const std::string& key) const;
-
-    void setValidationRule(const std::string& section, const std::string& key, const ValidationRules::Rule* rule);
-    void saveConfig() const;
-
-    virtual std::string getConfigFilePath() const = 0;
-    virtual std::vector<ConfigGen::ConfigSection> getConfigSections() const = 0;
-    
-    const std::unordered_map<std::string, ConfigSection>& getSections() const { return sections; }
-
-protected:
-    void loadConfig();
-    static std::string trim(const std::string& str);
-    void setValidationRules();
-    
-    std::string filepath;
-    std::unordered_map<std::string, ConfigSection> sections;
+	class ConfigReader {
+	public:
+		ConfigReader();
+		virtual ~ConfigReader() = default;
 	
-private:
-    void useDefaultValue(const std::string& section, const std::string& key, const ConfigGen::ConfigItem& item);
-    void setValueWithValidation(const std::string& section, const std::string& key, const std::string& value);
+		void initialize();
 	
-};
-
-void generateConfigFile(const ConfigReader& reader);
+		template<typename T>
+		T getValue(const std::string& section, const std::string& key) const;
+	
+		template<typename T>
+		void setValue(const std::string& section, const std::string& key, const T& value);
+	
+		bool hasValue(const std::string& section, const std::string& key) const;
+	
+		void saveConfig() const;
+	
+		virtual std::string getConfigFilePath() const = 0;
+		virtual std::vector<ConfigGen::ConfigSection> getConfigSections() const = 0;
+		
+		const std::unordered_map<std::string,std::unordered_map<std::string,std::unique_ptr<ConfigGen::ConfigItemBase>>>& getSections() const { return sections; }
+	
+	protected:
+		void loadConfig();
+		static std::string trim(const std::string& str);
+		
+		std::string filepath;
+		std::unordered_map<std::string, std::unordered_map<std::string, std::unique_ptr<ConfigGen::ConfigItemBase>>> sections;
+		
+	private:
+		void useDefaultValue(const std::string& section, const std::string& key, const ConfigGen::ConfigItemBase& item);
+		void setValueWithValidation(const std::string& section, const std::string& key, const std::string& value);
+		
+	};
+	
+	void generateConfigFile(const ConfigReader& reader);
 } // namespace ConfigLib
 
 #endif // CONFIG_READER_H
