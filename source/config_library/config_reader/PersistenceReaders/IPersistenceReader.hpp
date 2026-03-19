c8b9c4d18802c951a51ae5fa32b9a7f4c883cb90
Ian Tomes <mistourT@gmail.com>
2026-03-19 18:51:15 -0500

    adding an IPersistenceReader

---
+++ source/config_library/config_reader/PersistenceReaders/IPersistenceReader.hpp
@@ -0,0 +1,19 @@
+#pragma once
+
+namespace ConfigLib
+{
+	
+class IPersistenceReader
+{
+public:
+	virtual ~IPersistenceReader() = default;
+	
+	virtual void persistSave(const std::vector<ConfigSection>& configSections) const = 0;	
+	virtual void persistReset() = 0;
+	virtual void persistExport(
+		const std::string& exportPath,
+		const std::vector<ConfigSection>& configSections) const = 0;
+
+};
+	
+}
